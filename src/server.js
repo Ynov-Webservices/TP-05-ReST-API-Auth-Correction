d25fcb444c2a1bad77d7dc75afc904d9ea2a63af
MisterTakaashi <soapmctravich@gmail.com>
2020-04-10 15:50:06 +0200

    Added basic auth

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
 const router = require('./router');
 
 const app = express();
 let port = process.env.PORT || 3000;
+
+if (process.env.BASIC_AUTH) {
+  app.use(basicAuth({
+    users: { 'admin': 'supersecret' }
+  }));
+}
 
 router(app);
 
